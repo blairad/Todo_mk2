b94f2d10228ac97d1d476f1ead211613fb8c9c4f
Andrew Blair <blair.d.andrew@gmail.com>
2019-11-20 18:49:20 +0000

    some changes. things work on codepen but not local server

--- todo.js
+++ todo.js
@@ -1,5 +1,3 @@
-
-
 const form = document.querySelector('form');
 const todoList = document.querySelector('ul');
 const button = document.querySelector('button');
@@ -22,45 +20,36 @@ const todoMaker = function(text){
     todoItem.textContent = text;
     todoList.appendChild(todoItem); 
     todoItem.appendChild(checkbox)
-// // checkbox.checked = todo.complete;
-//     const span = document.createElement('span')
-//     span.contentEditable = true
-    // span.classList.add('editable')
-
- // If the todo is complete, it will have a strikethrough
-    // if (todo.complete) {
-    //   const strike = document.createElement('s')
-    //   strike.textContent = text
-    //   todoItem.append(strike)
-    // } else {
-    //   // Otherwise just display the text
-    //   todoItem.textContent = text
-    // }
-
+ 
 }
 function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
+  if (i < 10) {
+    i = "0" + i;
   }
+  return i;
+}
+
+function currentTime(){
+  const today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  minutes = checkTime(minutes);
   
-  function currentTime(){
-    const today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    minutes = checkTime(minutes);
-    
-    document.getElementById("container-time").innerHTML = hours + ":" + minutes;
-    currentTime = setTimeout(function() {
-      time()
-    }, 500);
-  }
-  currentTime();
+  document.getElementById("container-time").innerHTML = hours + ":" + minutes;
+  currentTime = setTimeout(function() {
+    time()
+  }, 500);
+}
+currentTime();
+// function changeID(checkbox) {
+//         checkbox.id = 'onCheck';
+//     }
 
 button.addEventListener('click', function(){
     while(todoList.firstChild){
         todoList.removeChild(todoList.firstChild)
     }
 })
+
+
 
